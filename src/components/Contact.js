58854814a49ceda54cde3faea2c99769ec44e51a
amit <amit2054128@gmail.com>
2022-11-29 19:14:29 +0545

    contact page created

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,29 @@ export default function Contact() {
   return (
     <>
     <Nav/>
-        <h1>hello gaurab</h1>
+        <div className="container">
+        <div className="card">
+          <div className="card-body">
+          <form>
+  <div className="mb-3">
+    <label className="form-label">Name</label>
+    <input type="text" className="form-control" id="name" name='name'/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Email</label>
+    <input type="email" className="form-control" id="email" name='email'/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Phone Number</label>
+    <input type="text" className="form-control" id="number" name='number'/>
+  </div>
+ 
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+          </div>
+        </div>
+
+        </div>
     </>
   )
 }
